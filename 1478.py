e983056e820995f7665f596f5681bbd4d3342db2
dmswl0311 <dmswl_0311@naver.com>
2020-12-31 22:02:12 +0900

    20-12-31

--- 1478.py
+++ 1478.py
@@ -1,13 +1,15 @@
 n, m = map(int, input().split())
 array = [[0]*m for _ in range(n)]
 num = 1
+flag=n
 
 for k in range(n+m-1):
     for i in range(n):
         for j in range(m-1, -1, -1):
-            if (i+j == n-i):
+            if (i+j == flag):
                 array[i][j] = num
                 num += 1
+                flag=(n-1)*2
             else:
                 continue
 
