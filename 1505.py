11dd92af59997e2dfa9323b3e3c7363957b00b32
dmswl0311 <dmswl_0311@naver.com>
2021-01-04 18:36:57 +0900

    1505 2차원 배열 채우기 3(달팽이 배열)

---
+++ 1505.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(sys.stdin.readline())
+array = [[0]*n for _ in range(n)]
+num = 1
+row = 0
+col = -1
+flag = 1
+result_n = n
+for i in range(n):
+    for _ in range(n):
+        col += flag
+        array[row][col] = num
+        num += 1
+    n = n-1
+    for _ in range(n):
+        row += flag
+        array[row][col] = num
+        num += 1
+    flag *= -1
+
+for i in range(result_n):
+    for j in range(result_n):
+        print(array[i][j], end=' ')
+    print(' ')
