7d2b343d0b5b0519d585275768170c9f33e207a1
dmswl0311 <dmswl_0311@naver.com>
2021-01-10 19:47:39 +0900

    1473 2차원 배열 지그재그 2-6

---
+++ 1472.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+array = [[0]*m for _ in range(n)]
+num = 1
+flag = 0
+for i in range(n-1, -1, -1):
+    if (flag % 2 == 0):
+        for j in range(m-1, -1, -1):
+            array[i][j] = num
+            num += 1
+    else:
+        for j in range(m):
+            array[i][j] = num
+            num += 1
+    flag += 1
+for i in range(n):
+    for j in range(m):
+        print(array[i][j], end=' ')
+    print(' ')
