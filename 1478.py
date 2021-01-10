d64d8a7cf15f7a511375d71dc6606d5da172ac6f
dmswl0311 <dmswl_0311@naver.com>
2021-01-10 19:28:00 +0900

    1478 오답

--- 1478.py
+++ 1478.py
@@ -1,12 +1,26 @@
 n, m = map(int, input().split())
 array = [[0]*m for _ in range(n)]
+
+row = 0
+col = m
 num = 1
+limit =
 
-for i in range(n):
-    for j in range(m-(1-i), m-(2-i), -1):
-        if(i+j == j):
-            array[i][j] = i+1
+for k in range(limit):
+    for i in range(0, k+1):
+        array[row+i][col+(i-1)] = num
+        num += 1
+    row = row
+    col = col-1
 
+row = 0
+col = 0
+for k in range(limit, 0, -1):
+    for i in range(0, k):
+        array[row+i][col+i] = num
+        num += 1
+    row = row+1
+    col = col
 
 for i in range(n):
     for j in range(m):
