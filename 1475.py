d7c66c2c10f88be589691f548b47fcf7d990fcda
dmswl0311 <dmswl_0311@naver.com>
2021-01-10 19:56:13 +0900

    1475 2차원 배열 지그재그 2-8

--- 1475.py
+++ 1475.py
@@ -1,18 +1,21 @@
-# 최대 1024 바이트?
+import sys
 
-n, m = map(int, input().split())
+n, m = map(int, sys.stdin.readline().split())
 array = [[0]*m for _ in range(n)]
-k = 1
 
-for i in range(m-1, -1, -1):
-    if (i % 2 == 0):
-        for j in range(n):
-            array[j][i] = k
-            k += 1
+num = 1
+flag = 1
+
+for j in range(m-1, -1, -1):
+    if (flag % 2 != 0):
+        for i in range(n):
+            array[i][j] = num
+            num += 1
     else:
-        for j in range(n-1, -1, -1):
-            array[j][i] = k
-            k += 1
+        for i in range(n-1, -1, -1):
+            array[i][j] = num
+            num += 1
+    flag += 1
 
 for i in range(n):
     for j in range(m):
