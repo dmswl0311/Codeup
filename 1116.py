695c2752adfcef47e2f9e77583cf19f5744cd375
dmswl0311 <dmswl_0311@naver.com>
2021-01-13 16:15:28 +0900

    1116 사칙연산 계산기

---
+++ 1116.py
@@ -0,0 +1,5 @@
+a, b = map(int, input().split())
+print("{0}+{1}={2}".format(a, b, a+b))
+print("{0}-{1}={2}".format(a, b, a-b))
+print("{0}*{1}={2}".format(a, b, a*b))
+print("{0}/{1}={2}".format(a, b, a//b))
