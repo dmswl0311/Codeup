c7c5c322f7cfeae12417fcc1765c2bf5b03abdee
dmswl0311 <dmswl_0311@naver.com>
2021-01-13 21:16:36 +0900

    3004 데이터 재정렬_이진탐색

--- 3004.py
+++ 3004.py
@@ -1,15 +1,23 @@
 import sys
 
 n = int(sys.stdin.readline())
-a = list(map(int, sys.stdin.readline().split()))
-result = [0]*n
+b = list(map(int, sys.stdin.readline().split()))
 
-for i in range(n):
-    num = 0
-    for j in range(n):
-        if a[i] > a[j]:
-            num += 1
-    result[i] = num
+a = sorted(b)
 
-for i in result:
-    print(i, end=' ')
+
+def search(a, target, low, high):
+    if low > high:
+        return -1
+    mid = (low+high)//2
+    if(a[mid] == target):
+        return mid
+    elif (a[mid] > target):
+        return search(a, target, low, mid-1)
+    else:
+        return search(a, target, mid+1, high)
+
+
+for i in b:
+    target = i
+    print(search(a, target, 0, n), end=' ')
