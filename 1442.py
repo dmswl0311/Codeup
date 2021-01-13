b3da2ebbde20fc790049225767ee3f40c3113935
dmswl0311 <dmswl_0311@naver.com>
2021-01-13 17:21:33 +0900

    1442 선택 정렬

---
+++ 1442.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+array = [int(sys.stdin.readline()) for _ in range(n)]
+
+for i in range(n):
+    least = i
+    for j in range(i+1, n):
+        if(array[least] > array[j]):
+            least = j
+    if (least != i):
+        array[i], array[least] = array[least], array[i]
+
+for i in array:
+    print(i)
+
