9274f8ce2f3d273d9570a5bd8547f63a5bc59df1
dmswl0311 <dmswl_0311@naver.com>
2021-01-13 18:47:15 +0900

    1441 버블소트 최적화

--- 1441.py
+++ 1441.py
@@ -1,18 +1,16 @@
 import sys
-import time
-start = time.time()
 n = int(sys.stdin.readline())
 array = [int(sys.stdin.readline()) for _ in range(n)]
 
-for i in range(n-1):
-    for j in range(n-i-1):
+end = n-1
+
+while(end > 0):
+    swap = 0
+    for j in range(end):
         if (array[j] > array[j+1]):
             array[j], array[j+1] = array[j+1], array[j]
-        else:
-            continue
+            swap = j
+        end = swap
 
 for i in array:
     print(i)
-
-end = time.time()
-print(end-start)
---
+++ 3011.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+array = list(map(int, sys.stdin.readline().split()))
+cnt = 0
+
+for i in range(n-1, 0, -1):
+    swap = 0
+    for j in range(i):
+        if (array[j] > array[j+1]):
+            array[j], array[j+1] = array[j+1], array[j]
+            swap += 1
+    cnt += 1
+    if swap == 0:
+        break
+
+print(cnt-1)
