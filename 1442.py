07a099ec82e30a8d248033852a7ef4ccfbdd926b
dmswl0311 <dmswl_0311@naver.com>
2021-01-16 16:32:05 +0900

    1442 선택정렬

--- 1442.py
+++ 1442.py
@@ -4,12 +4,12 @@ n = int(sys.stdin.readline())
 array = [int(sys.stdin.readline()) for _ in range(n)]
 
 for i in range(n-1):
-    least = i
+    min = i
     for j in range(i+1, n):
-        if(array[least] > array[j]):
-            least = j
-    if (least != i):
-        array[i], array[least] = array[least], array[i]
+        if(array[min] > array[j]):
+            min = j
+    if (min != i):
+        array[i], array[min] = array[min], array[i]
 
 for i in array:
     print(i)
