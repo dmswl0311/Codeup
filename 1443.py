bb61a92adce4330a814f9a2a425d52f27de60fa4
dmswl0311 <dmswl_0311@naver.com>
2021-01-16 16:30:20 +0900

    1443 삽입정렬

--- 1443.py
+++ 1443.py
@@ -10,6 +10,8 @@ for i in range(1, n):
     for j in range(i, 0, -1):
         if(array[j] < array[j-1]):
             array[j], array[j-1] = array[j-1], array[j]
+        else:
+            break
 
 for i in array:
     print(i)
