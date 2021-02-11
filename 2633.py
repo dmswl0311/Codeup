71fc43f6fac9a5aa37c40fd991c8b67cf5b656ce
dmswl0311 <dmswl_0311@naver.com>
2021-02-11 23:48:35 +0900

    [이진탐색] 2633 Lower Bound

--- 2633.py
+++ 2633.py
@@ -3,15 +3,20 @@ array = sorted(list(map(int, input().split())))
 result = 0
 start = 0
 end = n-1
+cnt = 0
 
 while(start <= end):
     mid = (start+end)//2
 
-    if k >= array[mid]:
+    if k <= array[mid]:
         result = mid
         end = mid-1
 
     else:
+        cnt += 1
         start = mid+1
 
-print(result)
+if cnt == ((n-1)//2)+1:
+    print(n+1)
+else:
+    print(result+1)
