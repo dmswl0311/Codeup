bfbca6ac8fc5b53f787f96b0d3f8bdea567499eb
dmswl0311 <dmswl_0311@naver.com>
2021-04-19 00:05:02 +0900

    [BFS] 4421 단지 번호 붙이기

--- 4421.py
+++ 4421.py
@@ -6,14 +6,15 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 result = 1
 
-print(graph)
-
 q = deque()
+result_cnt = []
 
 
 def bfs(x, y, result):
+    cnt = 0
     q.append((x, y))
     graph[x][y] = result
+    cnt += 1
     while (q):
         x, y = q.popleft()
         for i in range(4):
@@ -22,15 +23,19 @@ def bfs(x, y, result):
             if nx < 0 or ny < 0 or nx >= n or ny >= n:
                 continue
             if graph[nx][ny] == 1:
-                print("result= ", result, nx, ny)
                 graph[nx][ny] = result
+                cnt += 1
                 q.append((nx, ny))
+    result_cnt.append(cnt)
 
 
 for i in range(n):
     for j in range(n):
-        if graph[i][j] != 0:
-            bfs(i, j, result)
+        if graph[i][j] == 1:
             result += 1
+            bfs(i, j, result)
+print(result-1)
+result_cnt.sort()
 
-print(graph)
+for i in result_cnt:
+    print(i)
